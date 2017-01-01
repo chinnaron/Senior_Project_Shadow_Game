7d849ddd403ba33b401e3a66668a805d222e04ef
JChinnaron <chinnaron1@gmail.com>
2017-01-02 03:54:30 +0700

    100MB total, new model(50MB), new script, can't walk lol

--- Puzzle Game/Assets/Scripts/Grid.cs
+++ Puzzle Game/Assets/Scripts/Grid.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Grid : MonoBehaviour {
+	public int width = 20;
+	public int height = 20;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+	private GameObject[,] grid;
+
+	void Awake (){
+		grid = new GameObject[width, height];
+		for (int x = 0; x < width; x++) {
+			for (int z = 0; z < width; z++) {
+				
+			}
+		}
 	}
 }
--- Puzzle Game/Assets/Scripts/PlayerController.cs
+++ Puzzle Game/Assets/Scripts/PlayerController.cs
@@ -1,67 +1,112 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AI;
 
 public class PlayerController : MonoBehaviour {
 	
-	public float speed = 2f;
+	public float speed = 1f;
+	public GameObject floor;
+//	public GameObject light;
+
+	public int[,] grid;
+
+	private static int north = 0;
+	private static int east = 1;
+	private static int south = 2;
+	private static int west = 3;
 
 	private Animator anim;
-	private bool walking;
-//	private bool grabbing;
-	private int floorMask;
-	private float camRayLength = 400f;
-	private Vector3 movement;
 	private Rigidbody playerRigidbody;
-	private Vector3 destination;
+
+	private bool walking;
+	private bool grabbing;
+
+	private int floorMask;
+	private int unWalkableMask;
+	private int direction = north;
+	private float camRayLength = 100f;
+	private int[] destination;
+	private int[] position;
+
+	private Vector3 movement;
+	private Vector3 rayOrigin = new Vector3 (0f, 10f, 0f);
+	private Vector3 rayDirection = Vector3.down;
+
 	private Ray ray;
 	private RaycastHit hit;
-	private List<Vector3> path;
-	private NavMeshAgent navMeshAgent;
 
+//	private List<Vector3> path;
 
 	void Awake (){
 		floorMask = LayerMask.GetMask ("Floor");
-		destination = transform.position;
-
+		unWalkableMask = LayerMask.GetMask ("UnWalkable");
+		position = destination = toAInt (transform.position);
+		direction = (int)(transform.rotation.y % 360 / 90);
 
 		anim = GetComponent <Animator> ();
 		playerRigidbody = GetComponent <Rigidbody> ();
-		navMeshAgent = GetComponent <NavMeshAgent> ();
+
+		grid = new int[(int)floor.transform.localScale.x, (int)floor.transform.localScale.y];
+		ray = new Ray (rayOrigin, rayDirection);
+		for (int x = 0; x < grid.GetLength (0); x++) {
+			for (int z = 0; z < grid.GetLength (1); z++) {
+				rayOrigin.x = rayDirection.x = x;
+				rayOrigin.z = rayDirection.z = z;
+				ray.direction = rayDirection;
+				ray.origin = rayOrigin;
+				if (Physics.Raycast (ray, out hit, camRayLength, unWalkableMask))
+					grid [x, z] = 0;
+				else
+					grid [x, z] = 1;
+			}
+		}
 	}
 
 	void FixedUpdate (){
 		ray = Camera.main.ScreenPointToRay (Input.mousePosition);
 
+		if (Input.GetButtonDown ("Fire2")) {
+			grabbing = true;
+		}
+
 		Move (ray, hit);
-//		Turning (ray, hit);
+
 		Animating ();
 	}
 
 	void Move (Ray ray, RaycastHit hit){
-
 		if (Input.GetButtonDown ("Fire1") && Physics.Raycast(ray, out hit, camRayLength, floorMask)) {
 //			path = findPath (hit.point, transform.position);
 			walking = true;
-			navMeshAgent.destination = hit.point;
-			navMeshAgent.Resume ();
+			destination = toAInt (hit.point);
 		}
 
-		if (navMeshAgent.remainingDistance <= navMeshAgent.stoppingDistance && (!navMeshAgent.hasPath || Mathf.Abs (navMeshAgent.velocity.sqrMagnitude) < float.Epsilon)) {
+		if (destination == position) {
 			walking = false;
 		} else {
 			walking = true;
 		}
-	}
 
-//	void Turning (Ray ray, RaycastHit hit){
-//		
-//	}
+		movement = transform.position.normalized;
+		movement *= speed * Time.deltaTime;
+		playerRigidbody.MovePosition (transform.position + movement);
+	}
 
 	void Animating (){
 		anim.SetBool ("IsWalking", walking);
-//		anim.SetBool ("IsGrabbing", grabbing);
+		anim.SetBool ("IsGrabbing", grabbing);
+	}
+
+	int[] toAInt (Vector3 v){
+		return new int[] { (int)v.x, (int)v.y };
+	}
+		
+	bool isSamePoint (Vector3 a, Vector3 b){
+		if (a.x != b.x)
+			return false;
+		if (a.z != b.z)
+			return false;
+		return true;
 	}
 
 //	List<Vector3> findPath (Vector3 sta, Vector3 des){
