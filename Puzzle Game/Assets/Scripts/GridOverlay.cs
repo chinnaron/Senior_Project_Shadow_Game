fddf3f157fab0e1accc3ae1818581000ea54e411
JChinnaron <chinnaron1@gmail.com>
2017-01-05 02:43:37 +0700

    can walk, have grid texture, new model again, new grid script

---
+++ Puzzle Game/Assets/Scripts/GridOverlay.cs
@@ -0,0 +1,152 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridOverlay : MonoBehaviour {
+	public static GridOverlay instance;
+
+	private int[,] grid;
+
+	private int lengthX;
+	private int lengthZ;
+
+	private Vector3 start;
+
+	private Ray ray;
+	private RaycastHit hit;
+
+	private int floorMask;
+	private int unWalkableMask;
+
+	private Collider floorCollider;
+
+	void Awake (){
+		lengthX = (int)transform.localScale.x;
+		lengthZ = (int)transform.localScale.y;
+
+		start = transform.position - new Vector3 (lengthX / 2f, 0f, lengthZ / 2f);
+		start.y = 10f;
+		grid = new int[lengthX, lengthZ];
+		ray = new Ray (start, Vector3.down);
+
+		floorMask = LayerMask.GetMask ("Floor");
+		unWalkableMask = LayerMask.GetMask ("UnWalkable");
+
+		for (int x = 0; x < lengthX; x++) {
+			for (int z = 0; z < lengthZ; z++) {
+				ray.origin = new Vector3 (x + start.x + 0.5f, ray.origin.y, z + start.z + 0.5f);
+				if (Physics.Raycast (ray, out hit, 11f, unWalkableMask)) {
+					grid [x, z] = -1;
+				} else if (Physics.Raycast (ray, out hit, 11f, floorMask)) {
+					grid [x, z] = 0;
+				} else {
+					grid [x, z] = -1;
+				}
+			}
+		}
+	}
+
+	public int GetLengthX (){
+		return lengthX;
+	}
+
+	public int GetLengthZ (){
+		return lengthZ;
+	}
+
+	public void SetGrid (Vector3 idx, int v){
+		grid [(int)((idx.x > 0f ? (int)idx.x : (int)idx.x - 1f) - start.x), (int)((idx.z > 0f ? (int)idx.z : (int)idx.z - 1f) - start.z)] = v;
+	}
+
+	public void SetGrid (int x, int z, int v){
+		grid [x, z] = v;
+	}
+
+	public int GetGrid (Vector3 idx){
+		return grid [(int)((idx.x > 0f ? (int)idx.x : (int)idx.x - 1f) - start.x), (int)((idx.z > 0f ? (int)idx.z : (int)idx.z - 1f) - start.z)];
+	}
+		
+	public int GetGrid (int x, int z){
+		return grid [x, z];
+	}
+
+	public int toGrid (Vector3 idx, char c){
+		if (c == 'x')
+			return (int)((idx.x > 0f ? (int)idx.x : (int)idx.x - 1f) - start.x);
+		else
+			return (int)((idx.z > 0f ? (int)idx.z : (int)idx.z - 1f) - start.z);
+	}
+
+	public Vector3 GetPoint (Vector3 idx){
+		idx.x = (idx.x > 0f ? (int)idx.x : (int)idx.x - 1f) + 0.5f;
+		idx.z = (idx.z > 0f ? (int)idx.z : (int)idx.z - 1f) + 0.5f;
+		return idx;
+	}
+
+	public Vector3 GetPoint (int x, int z){
+		return new Vector3 (start.x + 0.5f + x, 0f, start.z + 0.5f + z);
+	}
+
+	Vector3 nearer (Vector3 a, Vector3 b, Vector3 des){
+		return Vector3.Distance (a, des) < Vector3.Distance (b, des) ? a : b;
+	}
+
+	public Queue<Vector3> findPath(Vector3 start, Vector3 stop){
+		start.x = (start.x > 0f ? (int)start.x : (int)start.x - 1f) + 0.5f;
+		start.z = (start.z > 0f ? (int)start.z : (int)start.z - 1f) + 0.5f;
+		stop.x = (stop.x > 0f ? (int)stop.x : (int)stop.x - 1f) + 0.5f;
+		stop.z = (stop.z > 0f ? (int)stop.z : (int)stop.z - 1f) + 0.5f;
+		Stack<Vector3> ans = new Stack<Vector3> ();
+		Queue<Vector3> q = new Queue<Vector3> ();
+		Vector3 now = start;
+		int[,] grid2 = grid;
+		int count = 0;
+		q.Enqueue (now);
+
+		while (now != stop) {
+			if (toGrid (now, 'z') > 0 && grid2 [toGrid (now, 'x'), toGrid (now, 'z') - 1] == 0) {
+				grid2 [toGrid (now, 'x'), toGrid (now, 'z') - 1] = count;
+				q.Enqueue (GetPoint (toGrid (now, 'x'), toGrid (now, 'z') - 1));
+			}
+			if (toGrid (now, 'z') <  lengthZ && grid2 [toGrid (now, 'x'), toGrid (now, 'z') + 1] == 0) {
+				grid2 [toGrid (now, 'x'), toGrid (now, 'z') + 1] = count;
+				q.Enqueue (GetPoint (toGrid (now, 'x'), toGrid (now, 'z') + 1));
+			}
+			if (toGrid (now, 'x') > 0 && grid2 [toGrid (now, 'x') - 1, toGrid (now, 'z')] == 0) {
+				grid2 [toGrid (now, 'x') - 1, toGrid (now, 'z')] = count;
+				q.Enqueue (GetPoint (toGrid (now, 'x') - 1, toGrid (now, 'z')));
+			}
+			if (toGrid (now, 'x') <  lengthX && grid2 [toGrid (now, 'x') + 1, toGrid (now, 'z')] == 0) {
+				grid2 [toGrid (now, 'x') + 1, toGrid (now, 'z')] = count;
+				q.Enqueue (GetPoint (toGrid (now, 'x') + 1, toGrid (now, 'z')));
+			}
+			q.Dequeue ();
+			if (q.Count < 1)
+				return ans;
+			now = q.Peek ();
+		}
+
+		count = GetGrid (now);
+		ans.Push (now);
+		Vector3 near = now;
+		while (ans.Peek () != start) {
+			count--;
+			if (toGrid (now, 'z') > 0 && grid2 [toGrid (now, 'x'), toGrid (now, 'z') - 1] == count) {
+				near = nearer (near, GetPoint (toGrid (now, 'x'), toGrid (now, 'z') - 1), start);
+			}
+			if (toGrid (now, 'z') <  lengthZ && grid2 [toGrid (now, 'x'), toGrid (now, 'z') + 1] == count) {
+				near = nearer (near, GetPoint (toGrid (now, 'x'), toGrid (now, 'z') + 1), start);
+			}
+			if (toGrid (now, 'x') > 0 && grid2 [toGrid (now, 'x') - 1, toGrid (now, 'z')] == count) {
+				near = nearer (near, GetPoint (toGrid (now, 'x') - 1, toGrid (now, 'z')), start);
+			}
+			if (toGrid (now, 'x') <  lengthX && grid2 [toGrid (now, 'x') + 1, toGrid (now, 'z')] == count) {
+				near = nearer (near, GetPoint (toGrid (now, 'x') + 1, toGrid (now, 'z')), start);
+			}
+			ans.Push (near);
+			now = near;
+		}
+
+		return ans;
+	}
+}
--- Puzzle Game/Assets/Scripts/PlayerController.cs
+++ Puzzle Game/Assets/Scripts/PlayerController.cs
@@ -3,17 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
-	
-	public float speed = 1f;
 	public GameObject floor;
-//	public GameObject light;
+	private GridOverlay grid;
 
-	public int[,] grid;
+	private float speed = 1.5f;
+	private float turnSpeed = 5f;
 
-	private static int north = 0;
-	private static int east = 1;
-	private static int south = 2;
-	private static int west = 3;
+	private int north = 0;
+	private int east = 1;
+	private int south = 2;
+	private int west = 3;
 
 	private Animator anim;
 	private Rigidbody playerRigidbody;
@@ -22,74 +21,63 @@ public class PlayerController : MonoBehaviour {
 	private bool grabbing;
 
 	private int floorMask;
-	private int unWalkableMask;
-	private int direction = north;
 	private float camRayLength = 100f;
-	private int[] destination;
-	private int[] position;
 
+	private Vector3 pathDestination;
 	private Vector3 movement;
-	private Vector3 rayOrigin = new Vector3 (0f, 10f, 0f);
-	private Vector3 rayDirection = Vector3.down;
+	private Vector3 destination;
+	private Vector3 point;
+
+	private Quaternion lookAt;
 
 	private Ray ray;
 	private RaycastHit hit;
 
-//	private List<Vector3> path;
 
-	void Awake (){
+	void Awake () {
 		floorMask = LayerMask.GetMask ("Floor");
-		unWalkableMask = LayerMask.GetMask ("UnWalkable");
-		position = destination = toAInt (transform.position);
-		direction = (int)(transform.rotation.y % 360 / 90);
+		destination = transform.position;
+		movement = Vector3.zero;
 
 		anim = GetComponent <Animator> ();
 		playerRigidbody = GetComponent <Rigidbody> ();
+//		grid = GetComponent <GridOverlay> ();
+		grid = FindObjectOfType(typeof(GridOverlay)) as GridOverlay;
+	}
 
-		grid = new int[(int)floor.transform.localScale.x, (int)floor.transform.localScale.y];
-		ray = new Ray (rayOrigin, rayDirection);
-		for (int x = 0; x < grid.GetLength (0); x++) {
-			for (int z = 0; z < grid.GetLength (1); z++) {
-				rayOrigin.x = rayDirection.x = x;
-				rayOrigin.z = rayDirection.z = z;
-				ray.direction = rayDirection;
-				ray.origin = rayOrigin;
-				if (Physics.Raycast (ray, out hit, camRayLength, unWalkableMask))
-					grid [x, z] = 0;
-				else
-					grid [x, z] = 1;
-			}
+	void Update (){
+		ray = Camera.main.ScreenPointToRay (Input.mousePosition);//Change Input to mobile ver.
+
+		if (Input.GetButtonDown ("Fire1") && Physics.Raycast (ray, out hit, camRayLength, floorMask) && grid.GetPoint (hit.point) != transform.position && grid.GetGrid (hit.point) == 0) {//Change Input to mobile ver.
+			point = grid.GetPoint (hit.point);
+			destination = point;
+			destination.y = 0f;
+			movement = point - transform.position;
+			movement.y = 0f;
+			lookAt = Quaternion.LookRotation(point - transform.position);
+			walking = true;
+		}
+
+		if (Input.GetButtonDown ("Fire1")) {
+			grabbing = !grabbing;
+		}
+
+		if (Vector3.Dot((destination - transform.position).normalized, movement.normalized) == -1f) {
+			transform.position = destination;
+			walking = false;
+			movement = Vector3.zero;
 		}
 	}
 
-	void FixedUpdate (){
-		ray = Camera.main.ScreenPointToRay (Input.mousePosition);
-
-		if (Input.GetButtonDown ("Fire2")) {
-			grabbing = true;
-		}
-
-		Move (ray, hit);
-
+	void FixedUpdate () {
+		Move ();
 		Animating ();
 	}
 
-	void Move (Ray ray, RaycastHit hit){
-		if (Input.GetButtonDown ("Fire1") && Physics.Raycast(ray, out hit, camRayLength, floorMask)) {
-//			path = findPath (hit.point, transform.position);
-			walking = true;
-			destination = toAInt (hit.point);
-		}
-
-		if (destination == position) {
-			walking = false;
-		} else {
-			walking = true;
-		}
-
-		movement = transform.position.normalized;
-		movement *= speed * Time.deltaTime;
+	void Move (){
+		movement = movement.normalized * speed * Time.deltaTime;
 		playerRigidbody.MovePosition (transform.position + movement);
+		playerRigidbody.MoveRotation (Quaternion.Lerp(transform.rotation, lookAt, Time.fixedDeltaTime * turnSpeed));
 	}
 
 	void Animating (){
@@ -97,40 +85,196 @@ public class PlayerController : MonoBehaviour {
 		anim.SetBool ("IsGrabbing", grabbing);
 	}
 
-	int[] toAInt (Vector3 v){
-		return new int[] { (int)v.x, (int)v.y };
-	}
-		
-	bool isSamePoint (Vector3 a, Vector3 b){
-		if (a.x != b.x)
-			return false;
-		if (a.z != b.z)
-			return false;
-		return true;
-	}
 
-//	List<Vector3> findPath (Vector3 sta, Vector3 des){
-//		sta.x = (int)sta.x;
-//		des.x = (int)des.x;
-//		sta.z = (int)sta.z;
-//		des.z = (int)des.z;
-//		int len = Mathf.Abs ((int)des.x - (int)sta.x);
-//		for (int i = 0; i < len; i++) {
-//			path.Add (new Vector3 (sta.x, 0f, sta.z));
-//			if (des.x < sta.x)
-//				sta.x--;
-//			else
-//				sta.x++;
-//		}
-//		len = Mathf.Abs ((int)des.z - (int)sta.z);
-//		for (int i = 0; i < len; i++) {
-//			path.Add (new Vector3 (sta.x, 0f, sta.z));
-//			if (des.z < sta.z)
-//				sta.z--;
-//			else
-//				sta.z++;
-//		}
-//
-//		return path;
-//	}
 }
+
+
+
+//void Awake ()
+//{
+//	floorMask = LayerMask.GetMask ("Floor");
+//	unWalkableMask = LayerMask.GetMask ("UnWalkable");
+//	walkableMask = LayerMask.GetMask ("Walkable");
+//	position = destination = toGridIndex (transform.position);
+//	direction = (int)(transform.rotation.y % 360 / 90);
+//	pathDestination = transform.position;
+//
+//	anim = GetComponent <Animator> ();
+//	playerRigidbody = GetComponent <Rigidbody> ();
+//
+//	grid = new int[(int)floor.transform.localScale.x, (int)floor.transform.localScale.y];
+//	ray = new Ray (new Vector3 (0f, 10f, 0f), Vector3.down);
+//
+//	for (int x = 0; x < grid.GetLength (0); x++) {
+//		for (int z = 0; z < grid.GetLength (1); z++) {
+//			ray.origin = new Vector3 (x, 10f, z);
+//			if (Physics.Raycast (ray, out hit, camRayLength, floorMask))
+//				grid [x, z] = 0;
+//			else
+//				grid [x, z] = -1;
+//		}
+//	}
+//}
+//
+//void FixedUpdate ()
+//{
+//	ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+//
+//	if (Input.GetButtonDown ("Fire2")) {
+//		grabbing = true;
+//	}
+//
+//	Move (ray, hit);
+//
+//	Animating ();
+//}
+//
+//void Move (Ray ray, RaycastHit hit)
+//{
+//	if (Input.GetButtonDown ("Fire1") && Physics.Raycast (ray, out hit, camRayLength, floorMask)) {
+//		path = findPath (position, toGridIndex (hit.point));
+//		if (path.Count > 0) {
+//			destination = toGridIndex (hit.point);
+//			pathDestination.Set (path.Peek () [0], pathDestination.y, path.Peek () [1]);
+//
+//			if (path.Peek () [0] > position [0]) {
+//				direction = north;
+//				movement = Vector3.forward;
+//			} else if (path.Peek () [0] < position [0]) {
+//				direction = south;
+//				movement = Vector3.back;
+//			} else if (path.Peek () [1] > position [1]) {
+//				direction = east;
+//				movement = Vector3.right;
+//			} else {
+//				direction = west;
+//				movement = Vector3.left;
+//			}
+//
+//			path.Pop ();
+//			walking = true;
+//		} else {
+//
+//		}
+//	}
+//
+//	if ((direction == north && transform.position.x >= pathDestination.x) || (direction == south && transform.position.x <= pathDestination.x)
+//		|| (direction == east && transform.position.z >= pathDestination.z) || (direction == west && transform.position.z <= pathDestination.z)) {
+//		transform.position = pathDestination;
+//
+//		if (path.Count > 0) {
+//			position = toGridIndex (transform.position);
+//			pathDestination.Set (path.Peek () [0], pathDestination.y, path.Peek () [1]);
+//			path.Pop ();
+//
+//			if (path.Peek () [0] > position [0]) {
+//				direction = north;
+//				movement = Vector3.forward;
+//			} else if (path.Peek () [0] < position [0]) {
+//				direction = south;
+//				movement = Vector3.back;
+//			} else if (path.Peek () [1] > position [1]) {
+//				direction = east;
+//				movement = Vector3.right;
+//			} else {
+//				direction = west;
+//				movement = Vector3.left;
+//			}
+//		}
+//	}
+//
+//	if (isEqual(destination, position)) {
+//		walking = false;
+//		movement = Vector3.zero;
+//	} else {
+//		walking = true;
+//		movement = movement * speed * Time.deltaTime;
+//		playerRigidbody.MovePosition (transform.position + movement);
+//	}
+//}
+//
+//void Animating ()
+//{
+//	anim.SetBool ("IsWalking", walking);
+//	anim.SetBool ("IsGrabbing", grabbing);
+//}
+//
+//int[] toGridIndex (Vector3 v)
+//{
+//	return new int[] { (int)(v.x - 0.5f), (int)(v.z - 0.5f) };
+//}
+//
+//bool isSamePoint (Vector3 a, Vector3 b)
+//{
+//	if (a.x != b.x)
+//		return false;
+//	if (a.z != b.z)
+//		return false;
+//	return true;
+//}
+//
+//int[] nearer (int[] a, int[] b, int[] des){
+//	return Mathf.Abs (des [0] - a [0]) + Mathf.Abs (des [1] - a [1]) <= Mathf.Abs (des [0] - b [0]) + Mathf.Abs (des [1] - b [1]) ? a : b;
+//}
+//
+//bool isEqual (int[]a, int[]b){
+//	if (a [0] != b [0] || a [1] != b [1])
+//		return false;
+//	return true;
+//}
+//
+//Stack<int[]> findPath (int[] sta, int[] des){
+//	Stack<int[]> ans = new Stack<int[]> ();
+//	Queue<int[]> q = new Queue<int[]> ();
+//	int[,] grid2 = grid;
+//	int[] pos = sta;
+//	int count = 0;
+//
+//	q.Enqueue (pos);
+//	while (!isEqual (pos, des)) {
+//		count++;
+//		if (pos [1] != 0 && grid2 [pos [0], pos [1] - 1] == 0) {
+//			grid2 [pos [0], pos [1] - 1] = count;
+//			q.Enqueue (new int[] { pos [0], pos [1] - 1 });
+//		}
+//		if (pos [1] != grid2.GetLength (1) - 1 && grid2 [pos [0], pos [1] + 1] == 0) {
+//			grid2 [pos [0], pos [1] + 1] = count;
+//			q.Enqueue (new int[] { pos [0], pos [1] + 1 });
+//		}
+//		if (pos [0] != 0 && grid2 [pos [0] - 1, pos [1]] == 0) {
+//			grid2 [pos [0] - 1, pos [1]] = count;
+//			q.Enqueue (new int[] { pos [0] - 1, pos [1] });
+//		}
+//		if (pos [0] != grid2.GetLength (0) - 1 && grid2 [pos [0] + 1, pos [1]] == 0) {
+//			grid2 [pos [0] + 1, pos [1]] = count;
+//			q.Enqueue (new int[] { pos [0] + 1, pos [1] });
+//		}
+//		q.Dequeue ();
+//		if (q.Count < 1)
+//			return ans;
+//		pos = q.Peek ();
+//	}
+//
+//	int[] near = pos;
+//	count = grid2 [pos [0], pos [1]];
+//	ans.Push (des);
+//	while (!isEqual (pos, sta)) {
+//		count--;
+//		if (pos [1] != 0 && grid2 [pos [0], pos [1] - 1] == count) {
+//			near = nearer (near, new int[] { pos [0], pos [1] - 1 }, sta);
+//		}
+//		if (pos [1] != grid2.GetLength (1) && grid2 [pos [0], pos [1] + 1] == count) {
+//			near = nearer (near, new int[] { pos [0], pos [1] + 1 }, sta);
+//		}
+//		if (pos [0] != 0 && grid2 [pos [0] - 1, pos [1]] == count) {
+//			near = nearer (near, new int[] { pos [0] - 1, pos [1]}, sta);
+//		}
+//		if (pos [0] != grid2.GetLength (0) && grid2 [pos [0] + 1, pos [1]] == count) {
+//			near = nearer (near, new int[] { pos [0] + 1, pos [1]}, sta);
+//		}
+//		ans.Push (near);
+//		pos = near;
+//	}
+//	ans.Pop ();
+//	return ans;
+//}
