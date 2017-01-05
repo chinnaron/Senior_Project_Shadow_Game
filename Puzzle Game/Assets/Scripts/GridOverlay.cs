463147fe545e236ae182a3bd18c28a9f01664bfb
JChinnaron <chinnaron1@gmail.com>
2017-01-05 20:58:06 +0700

    Can walk like grid law ai sus, new shortest path by A*

--- Puzzle Game/Assets/Scripts/GridOverlay.cs
+++ Puzzle Game/Assets/Scripts/GridOverlay.cs
@@ -62,91 +62,161 @@ public class GridOverlay : MonoBehaviour {
 		grid [x, z] = v;
 	}
 
-	public int GetGrid (Vector3 idx){
-		return grid [(int)((idx.x > 0f ? (int)idx.x : (int)idx.x - 1f) - start.x), (int)((idx.z > 0f ? (int)idx.z : (int)idx.z - 1f) - start.z)];
+	public int GetGrid (Vector3 v){
+		return grid [(int)((v.x > 0f ? (int)v.x : (int)v.x - 1f) - start.x), (int)((v.z > 0f ? (int)v.z : (int)v.z - 1f) - start.z)];
 	}
 		
 	public int GetGrid (int x, int z){
 		return grid [x, z];
 	}
 
-	public int toGrid (Vector3 idx, char c){
+	public int toGrid (Vector3 v, char c){
 		if (c == 'x')
-			return (int)((idx.x > 0f ? (int)idx.x : (int)idx.x - 1f) - start.x);
+			return (int)((v.x > 0f ? (int)v.x : (int)v.x - 1f) - start.x);
 		else
-			return (int)((idx.z > 0f ? (int)idx.z : (int)idx.z - 1f) - start.z);
+			return (int)((v.z > 0f ? (int)v.z : (int)v.z - 1f) - start.z);
 	}
 
-	public Vector3 GetPoint (Vector3 idx){
-		idx.x = (idx.x > 0f ? (int)idx.x : (int)idx.x - 1f) + 0.5f;
-		idx.z = (idx.z > 0f ? (int)idx.z : (int)idx.z - 1f) + 0.5f;
-		return idx;
+	public Vector3 toPoint (Vector3 v){
+		v.x = (v.x > 0f ? (int)v.x : (int)v.x - 1f) + 0.5f;
+		v.z = (v.z > 0f ? (int)v.z : (int)v.z - 1f) + 0.5f;
+		return v;
 	}
 
-	public Vector3 GetPoint (int x, int z){
+	public Vector3 toPoint (int x, int z){
 		return new Vector3 (start.x + 0.5f + x, 0f, start.z + 0.5f + z);
 	}
-
-	Vector3 nearer (Vector3 a, Vector3 b, Vector3 des){
-		return Vector3.Distance (a, des) < Vector3.Distance (b, des) ? a : b;
+		
+	public List<Vector3> neighborOf (Vector3 v){
+		List<Vector3> l = new List<Vector3> ();
+		int gridX = toGrid (v, 'x');
+		int gridZ = toGrid (v, 'z');
+		if (gridX > 0 && grid [gridX - 1, gridZ] == 0)
+			l.Add (v + Vector3.left);
+		if (gridX < lengthX - 1 && grid [gridX + 1, gridZ] == 0)
+			l.Add (v + Vector3.right);
+		if (gridZ > 0 && grid [gridX, gridZ - 1] == 0)
+			l.Add (v + Vector3.back);
+		if (gridZ < lengthZ - 1 && grid [gridX, gridZ + 1] == 0)
+			l.Add (v + Vector3.forward);
+		return l;
 	}
 
-	public Queue<Vector3> findPath(Vector3 start, Vector3 stop){
-		start.x = (start.x > 0f ? (int)start.x : (int)start.x - 1f) + 0.5f;
-		start.z = (start.z > 0f ? (int)start.z : (int)start.z - 1f) + 0.5f;
-		stop.x = (stop.x > 0f ? (int)stop.x : (int)stop.x - 1f) + 0.5f;
-		stop.z = (stop.z > 0f ? (int)stop.z : (int)stop.z - 1f) + 0.5f;
+	public Stack<Vector3> findPath(Vector3 start, Vector3 goal){
 		Stack<Vector3> ans = new Stack<Vector3> ();
-		Queue<Vector3> q = new Queue<Vector3> ();
-		Vector3 now = start;
-		int[,] grid2 = grid;
-		int count = 0;
-		q.Enqueue (now);
+		HashSet<Vector3> closedSet = new HashSet<Vector3> ();
+		HashSet<Vector3> openSet = new HashSet<Vector3> ();
+		openSet.Add (start);
+		Dictionary<Vector3,Vector3> cameFrom = new Dictionary<Vector3, Vector3> ();
+		Dictionary<Vector3, float> gScore = new Dictionary<Vector3, float> ();
+		gScore [start] = 0;
+		Dictionary<Vector3, float> fScore = new Dictionary<Vector3, float> ();
+		fScore [start] = Vector3.Distance (start, goal);
+		List<Vector3> l = new List<Vector3> ();
+		Vector3 current = start;
+		float tGScore;
 
-		while (now != stop) {
-			if (toGrid (now, 'z') > 0 && grid2 [toGrid (now, 'x'), toGrid (now, 'z') - 1] == 0) {
-				grid2 [toGrid (now, 'x'), toGrid (now, 'z') - 1] = count;
-				q.Enqueue (GetPoint (toGrid (now, 'x'), toGrid (now, 'z') - 1));
+		while (openSet.Count > 0) {
+			float lowest = float.MaxValue;
+			foreach(Vector3 v in openSet){
+				if (lowest > fScore [v]) {
+					lowest = fScore [v];
+					current = v;
+				}
 			}
-			if (toGrid (now, 'z') <  lengthZ && grid2 [toGrid (now, 'x'), toGrid (now, 'z') + 1] == 0) {
-				grid2 [toGrid (now, 'x'), toGrid (now, 'z') + 1] = count;
-				q.Enqueue (GetPoint (toGrid (now, 'x'), toGrid (now, 'z') + 1));
-			}
-			if (toGrid (now, 'x') > 0 && grid2 [toGrid (now, 'x') - 1, toGrid (now, 'z')] == 0) {
-				grid2 [toGrid (now, 'x') - 1, toGrid (now, 'z')] = count;
-				q.Enqueue (GetPoint (toGrid (now, 'x') - 1, toGrid (now, 'z')));
-			}
-			if (toGrid (now, 'x') <  lengthX && grid2 [toGrid (now, 'x') + 1, toGrid (now, 'z')] == 0) {
-				grid2 [toGrid (now, 'x') + 1, toGrid (now, 'z')] = count;
-				q.Enqueue (GetPoint (toGrid (now, 'x') + 1, toGrid (now, 'z')));
-			}
-			q.Dequeue ();
-			if (q.Count < 1)
+
+			if (goal == current) {
+				ans.Push (current);
+				while (cameFrom.ContainsKey (current)) {
+					current = cameFrom [current];
+					ans.Push (current);
+				}
 				return ans;
-			now = q.Peek ();
-		}
+			}
 
-		count = GetGrid (now);
-		ans.Push (now);
-		Vector3 near = now;
-		while (ans.Peek () != start) {
-			count--;
-			if (toGrid (now, 'z') > 0 && grid2 [toGrid (now, 'x'), toGrid (now, 'z') - 1] == count) {
-				near = nearer (near, GetPoint (toGrid (now, 'x'), toGrid (now, 'z') - 1), start);
-			}
-			if (toGrid (now, 'z') <  lengthZ && grid2 [toGrid (now, 'x'), toGrid (now, 'z') + 1] == count) {
-				near = nearer (near, GetPoint (toGrid (now, 'x'), toGrid (now, 'z') + 1), start);
-			}
-			if (toGrid (now, 'x') > 0 && grid2 [toGrid (now, 'x') - 1, toGrid (now, 'z')] == count) {
-				near = nearer (near, GetPoint (toGrid (now, 'x') - 1, toGrid (now, 'z')), start);
-			}
-			if (toGrid (now, 'x') <  lengthX && grid2 [toGrid (now, 'x') + 1, toGrid (now, 'z')] == count) {
-				near = nearer (near, GetPoint (toGrid (now, 'x') + 1, toGrid (now, 'z')), start);
-			}
-			ans.Push (near);
-			now = near;
-		}
+			openSet.Remove (current);
+			closedSet.Add (current);
 
+			l = neighborOf (current);
+			foreach(Vector3 neighbor in l){
+				if (closedSet.Contains (neighbor))
+					continue;
+				
+				tGScore = gScore [current] + 1f;
+				if (!openSet.Contains (current))
+					openSet.Add (neighbor);
+				else if (tGScore >= gScore [neighbor])
+					continue;
+				
+				cameFrom [neighbor] = current;
+				gScore [neighbor] = tGScore;
+				fScore [neighbor] = gScore [neighbor] + Vector3.Distance (neighbor, goal);
+			}
+			l.Clear ();
+		}
 		return ans;
 	}
+
+//	Vector3 nearer (Vector3 a, Vector3 b, Vector3 des){
+//		return Vector3.Distance (a, des) < Vector3.Distance (b, des) ? a : b;
+//	}
+
+//	public Stack<Vector3> findPath(Vector3 start, Vector3 stop){
+//		start.x = (start.x > 0f ? (int)start.x : (int)start.x - 1f) + 0.5f;
+//		start.z = (start.z > 0f ? (int)start.z : (int)start.z - 1f) + 0.5f;
+//		stop.x = (stop.x > 0f ? (int)stop.x : (int)stop.x - 1f) + 0.5f;
+//		stop.z = (stop.z > 0f ? (int)stop.z : (int)stop.z - 1f) + 0.5f;
+//		Stack<Vector3> ans = new Stack<Vector3> ();
+//		Queue<Vector3> q = new Queue<Vector3> ();
+//		Vector3 now = start;
+//		int[,] grid2 = grid;
+//		int count = 0;
+//		q.Enqueue (now);
+//
+//		while (now != stop) {
+//			if (toGrid (now, 'z') > 0 && grid2 [toGrid (now, 'x'), toGrid (now, 'z') - 1] == 0) {
+//				grid2 [toGrid (now, 'x'), toGrid (now, 'z') - 1] = count;
+//				q.Enqueue (GetPoint (toGrid (now, 'x'), toGrid (now, 'z') - 1));
+//			}
+//			if (toGrid (now, 'z') <  lengthZ && grid2 [toGrid (now, 'x'), toGrid (now, 'z') + 1] == 0) {
+//				grid2 [toGrid (now, 'x'), toGrid (now, 'z') + 1] = count;
+//				q.Enqueue (GetPoint (toGrid (now, 'x'), toGrid (now, 'z') + 1));
+//			}
+//			if (toGrid (now, 'x') > 0 && grid2 [toGrid (now, 'x') - 1, toGrid (now, 'z')] == 0) {
+//				grid2 [toGrid (now, 'x') - 1, toGrid (now, 'z')] = count;
+//				q.Enqueue (GetPoint (toGrid (now, 'x') - 1, toGrid (now, 'z')));
+//			}
+//			if (toGrid (now, 'x') <  lengthX && grid2 [toGrid (now, 'x') + 1, toGrid (now, 'z')] == 0) {
+//				grid2 [toGrid (now, 'x') + 1, toGrid (now, 'z')] = count;
+//				q.Enqueue (GetPoint (toGrid (now, 'x') + 1, toGrid (now, 'z')));
+//			}
+//			q.Dequeue ();
+//			if (q.Count < 1)
+//				return ans;
+//			now = q.Peek ();
+//		}
+//
+//		count = GetGrid (now);
+//		ans.Push (now);
+//		Vector3 near = now;
+//		while (ans.Peek () != start) {
+//			count--;
+//			if (toGrid (now, 'z') > 0 && grid2 [toGrid (now, 'x'), toGrid (now, 'z') - 1] == count) {
+//				near = nearer (near, GetPoint (toGrid (now, 'x'), toGrid (now, 'z') - 1), start);
+//			}
+//			if (toGrid (now, 'z') <  lengthZ && grid2 [toGrid (now, 'x'), toGrid (now, 'z') + 1] == count) {
+//				near = nearer (near, GetPoint (toGrid (now, 'x'), toGrid (now, 'z') + 1), start);
+//			}
+//			if (toGrid (now, 'x') > 0 && grid2 [toGrid (now, 'x') - 1, toGrid (now, 'z')] == count) {
+//				near = nearer (near, GetPoint (toGrid (now, 'x') - 1, toGrid (now, 'z')), start);
+//			}
+//			if (toGrid (now, 'x') <  lengthX && grid2 [toGrid (now, 'x') + 1, toGrid (now, 'z')] == count) {
+//				near = nearer (near, GetPoint (toGrid (now, 'x') + 1, toGrid (now, 'z')), start);
+//			}
+//			ans.Push (near);
+//			now = near;
+//		}
+//
+//		return ans;
+//	}
 }
--- Puzzle Game/Assets/Scripts/PlayerController.cs
+++ Puzzle Game/Assets/Scripts/PlayerController.cs
@@ -33,6 +33,7 @@ public class PlayerController : MonoBehaviour {
 	private Ray ray;
 	private RaycastHit hit;
 
+	private Stack<Vector3> path = new Stack<Vector3> ();
 
 	void Awake () {
 		floorMask = LayerMask.GetMask ("Floor");
@@ -41,31 +42,46 @@ public class PlayerController : MonoBehaviour {
 
 		anim = GetComponent <Animator> ();
 		playerRigidbody = GetComponent <Rigidbody> ();
-//		grid = GetComponent <GridOverlay> ();
 		grid = FindObjectOfType(typeof(GridOverlay)) as GridOverlay;
 	}
 
 	void Update (){
 		ray = Camera.main.ScreenPointToRay (Input.mousePosition);//Change Input to mobile ver.
 
-		if (Input.GetButtonDown ("Fire1") && Physics.Raycast (ray, out hit, camRayLength, floorMask) && grid.GetPoint (hit.point) != transform.position && grid.GetGrid (hit.point) == 0) {//Change Input to mobile ver.
-			point = grid.GetPoint (hit.point);
-			destination = point;
-			destination.y = 0f;
-			movement = point - transform.position;
-			movement.y = 0f;
-			lookAt = Quaternion.LookRotation(point - transform.position);
-			walking = true;
+		if (Input.GetButtonDown ("Fire1") && Physics.Raycast (ray, out hit, camRayLength, floorMask) && grid.toPoint (hit.point) != transform.position && grid.GetGrid (hit.point) == 0) {//Change Input to mobile ver.
+			point = grid.toPoint (hit.point);
+			point.y = 0f;
+			path = grid.findPath (grid.toPoint (transform.position), point);
+			if (path.Count > 0) {
+				destination = point;
+				path.Pop ();
+				pathDestination = path.Peek ();
+				movement = pathDestination - transform.position;
+				lookAt = Quaternion.LookRotation (pathDestination - transform.position);
+				walking = true;
+			}
 		}
 
-		if (Input.GetButtonDown ("Fire1")) {
+		if (Input.GetButtonDown ("Fire2")) {
 			grabbing = !grabbing;
 		}
 
 		if (Vector3.Dot((destination - transform.position).normalized, movement.normalized) == -1f) {
 			transform.position = destination;
-			walking = false;
 			movement = Vector3.zero;
+			path.Clear ();
+			walking = false;
+		}
+
+		if (Vector3.Dot ((pathDestination - transform.position).normalized, movement.normalized) == -1f) {
+			if (path.Count > 0) {
+				transform.position = pathDestination;
+				path.Pop ();
+				pathDestination = path.Peek ();
+				movement = pathDestination - transform.position;
+				lookAt = Quaternion.LookRotation (pathDestination - transform.position);
+				walking = true;
+			}
 		}
 	}
 
@@ -84,197 +100,4 @@ public class PlayerController : MonoBehaviour {
 		anim.SetBool ("IsWalking", walking);
 		anim.SetBool ("IsGrabbing", grabbing);
 	}
-
-
 }
-
-
-
-//void Awake ()
-//{
-//	floorMask = LayerMask.GetMask ("Floor");
-//	unWalkableMask = LayerMask.GetMask ("UnWalkable");
-//	walkableMask = LayerMask.GetMask ("Walkable");
-//	position = destination = toGridIndex (transform.position);
-//	direction = (int)(transform.rotation.y % 360 / 90);
-//	pathDestination = transform.position;
-//
-//	anim = GetComponent <Animator> ();
-//	playerRigidbody = GetComponent <Rigidbody> ();
-//
-//	grid = new int[(int)floor.transform.localScale.x, (int)floor.transform.localScale.y];
-//	ray = new Ray (new Vector3 (0f, 10f, 0f), Vector3.down);
-//
-//	for (int x = 0; x < grid.GetLength (0); x++) {
-//		for (int z = 0; z < grid.GetLength (1); z++) {
-//			ray.origin = new Vector3 (x, 10f, z);
-//			if (Physics.Raycast (ray, out hit, camRayLength, floorMask))
-//				grid [x, z] = 0;
-//			else
-//				grid [x, z] = -1;
-//		}
-//	}
-//}
-//
-//void FixedUpdate ()
-//{
-//	ray = Camera.main.ScreenPointToRay (Input.mousePosition);
-//
-//	if (Input.GetButtonDown ("Fire2")) {
-//		grabbing = true;
-//	}
-//
-//	Move (ray, hit);
-//
-//	Animating ();
-//}
-//
-//void Move (Ray ray, RaycastHit hit)
-//{
-//	if (Input.GetButtonDown ("Fire1") && Physics.Raycast (ray, out hit, camRayLength, floorMask)) {
-//		path = findPath (position, toGridIndex (hit.point));
-//		if (path.Count > 0) {
-//			destination = toGridIndex (hit.point);
-//			pathDestination.Set (path.Peek () [0], pathDestination.y, path.Peek () [1]);
-//
-//			if (path.Peek () [0] > position [0]) {
-//				direction = north;
-//				movement = Vector3.forward;
-//			} else if (path.Peek () [0] < position [0]) {
-//				direction = south;
-//				movement = Vector3.back;
-//			} else if (path.Peek () [1] > position [1]) {
-//				direction = east;
-//				movement = Vector3.right;
-//			} else {
-//				direction = west;
-//				movement = Vector3.left;
-//			}
-//
-//			path.Pop ();
-//			walking = true;
-//		} else {
-//
-//		}
-//	}
-//
-//	if ((direction == north && transform.position.x >= pathDestination.x) || (direction == south && transform.position.x <= pathDestination.x)
-//		|| (direction == east && transform.position.z >= pathDestination.z) || (direction == west && transform.position.z <= pathDestination.z)) {
-//		transform.position = pathDestination;
-//
-//		if (path.Count > 0) {
-//			position = toGridIndex (transform.position);
-//			pathDestination.Set (path.Peek () [0], pathDestination.y, path.Peek () [1]);
-//			path.Pop ();
-//
-//			if (path.Peek () [0] > position [0]) {
-//				direction = north;
-//				movement = Vector3.forward;
-//			} else if (path.Peek () [0] < position [0]) {
-//				direction = south;
-//				movement = Vector3.back;
-//			} else if (path.Peek () [1] > position [1]) {
-//				direction = east;
-//				movement = Vector3.right;
-//			} else {
-//				direction = west;
-//				movement = Vector3.left;
-//			}
-//		}
-//	}
-//
-//	if (isEqual(destination, position)) {
-//		walking = false;
-//		movement = Vector3.zero;
-//	} else {
-//		walking = true;
-//		movement = movement * speed * Time.deltaTime;
-//		playerRigidbody.MovePosition (transform.position + movement);
-//	}
-//}
-//
-//void Animating ()
-//{
-//	anim.SetBool ("IsWalking", walking);
-//	anim.SetBool ("IsGrabbing", grabbing);
-//}
-//
-//int[] toGridIndex (Vector3 v)
-//{
-//	return new int[] { (int)(v.x - 0.5f), (int)(v.z - 0.5f) };
-//}
-//
-//bool isSamePoint (Vector3 a, Vector3 b)
-//{
-//	if (a.x != b.x)
-//		return false;
-//	if (a.z != b.z)
-//		return false;
-//	return true;
-//}
-//
-//int[] nearer (int[] a, int[] b, int[] des){
-//	return Mathf.Abs (des [0] - a [0]) + Mathf.Abs (des [1] - a [1]) <= Mathf.Abs (des [0] - b [0]) + Mathf.Abs (des [1] - b [1]) ? a : b;
-//}
-//
-//bool isEqual (int[]a, int[]b){
-//	if (a [0] != b [0] || a [1] != b [1])
-//		return false;
-//	return true;
-//}
-//
-//Stack<int[]> findPath (int[] sta, int[] des){
-//	Stack<int[]> ans = new Stack<int[]> ();
-//	Queue<int[]> q = new Queue<int[]> ();
-//	int[,] grid2 = grid;
-//	int[] pos = sta;
-//	int count = 0;
-//
-//	q.Enqueue (pos);
-//	while (!isEqual (pos, des)) {
-//		count++;
-//		if (pos [1] != 0 && grid2 [pos [0], pos [1] - 1] == 0) {
-//			grid2 [pos [0], pos [1] - 1] = count;
-//			q.Enqueue (new int[] { pos [0], pos [1] - 1 });
-//		}
-//		if (pos [1] != grid2.GetLength (1) - 1 && grid2 [pos [0], pos [1] + 1] == 0) {
-//			grid2 [pos [0], pos [1] + 1] = count;
-//			q.Enqueue (new int[] { pos [0], pos [1] + 1 });
-//		}
-//		if (pos [0] != 0 && grid2 [pos [0] - 1, pos [1]] == 0) {
-//			grid2 [pos [0] - 1, pos [1]] = count;
-//			q.Enqueue (new int[] { pos [0] - 1, pos [1] });
-//		}
-//		if (pos [0] != grid2.GetLength (0) - 1 && grid2 [pos [0] + 1, pos [1]] == 0) {
-//			grid2 [pos [0] + 1, pos [1]] = count;
-//			q.Enqueue (new int[] { pos [0] + 1, pos [1] });
-//		}
-//		q.Dequeue ();
-//		if (q.Count < 1)
-//			return ans;
-//		pos = q.Peek ();
-//	}
-//
-//	int[] near = pos;
-//	count = grid2 [pos [0], pos [1]];
-//	ans.Push (des);
-//	while (!isEqual (pos, sta)) {
-//		count--;
-//		if (pos [1] != 0 && grid2 [pos [0], pos [1] - 1] == count) {
-//			near = nearer (near, new int[] { pos [0], pos [1] - 1 }, sta);
-//		}
-//		if (pos [1] != grid2.GetLength (1) && grid2 [pos [0], pos [1] + 1] == count) {
-//			near = nearer (near, new int[] { pos [0], pos [1] + 1 }, sta);
-//		}
-//		if (pos [0] != 0 && grid2 [pos [0] - 1, pos [1]] == count) {
-//			near = nearer (near, new int[] { pos [0] - 1, pos [1]}, sta);
-//		}
-//		if (pos [0] != grid2.GetLength (0) && grid2 [pos [0] + 1, pos [1]] == count) {
-//			near = nearer (near, new int[] { pos [0] + 1, pos [1]}, sta);
-//		}
-//		ans.Push (near);
-//		pos = near;
-//	}
-//	ans.Pop ();
-//	return ans;
-//}
