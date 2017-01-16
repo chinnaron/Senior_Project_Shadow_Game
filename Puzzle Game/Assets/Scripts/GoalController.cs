d10e150a63336ff1eb60d66b7c49bed49489fcb0
KuroRyu029 <teerawit029@gmail.com>
2017-01-16 14:49:53 +0700

    Added Box and Plane Goal

---
+++ Puzzle Game/Assets/Scripts/GoalController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalController : MonoBehaviour {
+
+	//When player stand NEXT to collider box
+	void OnTriggerEnter(Collider other){
+		Debug.Log ("Goal Collided");
+		Application.LoadLevel (Application.loadedLevel);
+	}
+}
