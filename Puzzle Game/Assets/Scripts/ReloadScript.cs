4021e5a82d03cfadec590947bae3e6611c465edc
Suraphoj <suraphojb@gmail.com>
2017-01-16 15:01:14 +0700

    UI : now we have reload Button

---
+++ Puzzle Game/Assets/Scripts/ReloadScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ReloadScript : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+		
+		Button btn = GetComponent<Button>();
+		btn.onClick.AddListener(TaskOnClick);
+	}
+
+
+	void TaskOnClick(){
+		Debug.Log ("You have clicked the button!");
+		Application.LoadLevel (Application.loadedLevel);
+	}
+	// Update is called once per frame
+
+}
