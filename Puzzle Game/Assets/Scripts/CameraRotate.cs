4296950c3560abb10a5480fb6e5847ad88b3da19
Suraphoj <suraphojb@gmail.com>
2017-01-16 16:51:09 +0700

    update rotate button(still can't work)

--- Puzzle Game/Assets/Scripts/CameraRotate.cs
+++ Puzzle Game/Assets/Scripts/CameraRotate.cs
@@ -3,33 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class MenuScript : MonoBehaviour {
+public class CameraRotate : MonoBehaviour {
 
 	// Use this for initialization
-
-	public bool _isOpened;
 	void Start () {
 
-		_isOpened = false;
 		Button btn = GetComponent<Button>();
 		btn.onClick.AddListener(TaskOnClick);
-
 	}
 
 
 	void TaskOnClick(){
-
-		if (!_isOpened) {
-			//Debug.Log ("You have clicked the button!");
-			Time.timeScale = 0;
-			//OpenMenu()
-		}
-		else Time.timeScale = 1;
-		
-		_isOpened = !_isOpened;
-			//CloseMenu()
-		
 		//Debug.Log ("You have clicked the button!");
 
+
+		Camera.main.transform.Rotate (new Vector3 (90, 0, 0));
 	}
 }
