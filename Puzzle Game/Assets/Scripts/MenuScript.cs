df95294dd9a774c25fff489646f87f7f1f561061
Suraphoj <suraphojb@gmail.com>
2017-01-16 15:47:32 +0700

    UI :      Reload : can work      Menu : can pause/unpause game still no popup menu

--- Puzzle Game/Assets/Scripts/MenuScript.cs
+++ Puzzle Game/Assets/Scripts/MenuScript.cs
@@ -3,20 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ReloadScript : MonoBehaviour {
-	
+public class MenuScript : MonoBehaviour {
+
 	// Use this for initialization
+
+	public bool _isOpened;
 	void Start () {
-		
+
+		_isOpened = false;
 		Button btn = GetComponent<Button>();
 		btn.onClick.AddListener(TaskOnClick);
+
 	}
 
 
 	void TaskOnClick(){
-		//Debug.Log ("You have clicked the button!");
-		Application.LoadLevel (Application.loadedLevel);
-	}
-	// Update is called once per frame
 
+		if (!_isOpened) {
+			Debug.Log ("You have clicked the button!");
+			Time.timeScale = 0;
+			//OpenMenu()
+		}
+		else Time.timeScale = 1;
+		
+		_isOpened = !_isOpened;
+			//CloseMenu()
+		
+		//Debug.Log ("You have clicked the button!");
+
+	}
 }
