d5508cdb53914b027c158fcef765a7ddda5cc5e9
KuroRyu029 <teerawit029@gmail.com>
2017-01-26 20:24:38 +0700

    trying to make lever

---
+++ Puzzle Game/Assets/Scripts/LeverController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeverController : MonoBehaviour {
+
+	public Object light;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
