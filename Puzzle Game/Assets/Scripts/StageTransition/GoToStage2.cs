e6a12944665fdeec6732238fa6c138d6c0ec1947
Suraphoj <suraphojb@gmail.com>
2017-01-28 15:41:16 +0700

    TON : Stage 1-2 done

---
+++ Puzzle Game/Assets/Scripts/StageTransition/GoToStage2.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GoToStage2 : MonoBehaviour {
+
+	//When player stand NEXT to collider box
+	void OnTriggerEnter(Collider other){
+		Debug.Log ("Goal Collided");
+		SceneManager.LoadScene("Scene2");
+	}
+}
