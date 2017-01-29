5c30ed80cfeaf1b157d2008a19e3419755c6dc9e
JChinnaron <chinnaron1@gmail.com>
2017-01-29 19:40:06 +0700

    Merge remote-tracking branch 'origin/tpc'
    # Conflicts:
    #	Puzzle Game/Assets/Scenes/Scene.unity
    #	Puzzle Game/Assets/StreamingAssets/build_info.txt
    #	Puzzle Game/Library/CurrentLayout.dwlt
    #	Puzzle Game/Library/InspectorExpandedItems.asset
    #	Puzzle Game/Library/ScriptAssemblies/Assembly-CSharp-Editor.dll.mdb
    #	Puzzle Game/Library/ScriptAssemblies/Assembly-CSharp.dll
    #	Puzzle Game/Library/ScriptAssemblies/Assembly-CSharp.dll.mdb
    #	Puzzle Game/Library/assetDatabase3
    #	Puzzle Game/Library/expandedItems
    #	Puzzle Game/Library/metadata/00/00000000000000002000000000000000
    #	Puzzle Game/Library/metadata/00/00000000000000003000000000000000
    #	Puzzle Game/Library/metadata/00/00000000000000004000000000000000
    #	Puzzle Game/Library/metadata/00/00000000000000004100000000000000
    #	Puzzle Game/Library/metadata/00/00000000000000005000000000000000
    #	Puzzle Game/Library/metadata/00/00000000000000005100000000000000
    #	Puzzle Game/Library/metadata/00/00000000000000006000000000000000
    #	Puzzle Game/Library/metadata/00/00000000000000006100000000000000
    #	Puzzle Game/Library/metadata/00/00000000000000007000000000000000
    #	Puzzle Game/Library/metadata/00/00000000000000007100000000000000
    #	Puzzle Game/Library/metadata/00/00000000000000008000000000000000
    #	Puzzle Game/Library/metadata/00/00000000000000009000000000000000
    #	Puzzle Game/Library/metadata/00/0000000000000000a000000000000000
    #	Puzzle Game/Library/metadata/00/0000000000000000a100000000000000
    #	Puzzle Game/Library/metadata/00/0000000000000000b000000000000000
    #	Puzzle Game/Library/metadata/00/0000000000000000c000000000000000
    #	Puzzle Game/Library/metadata/12/12fd8a0055b84bb59e84c9835a37e333
    #	Puzzle Game/Library/metadata/1c/1c6d1fbb51834b64847b1b73a75bfc77
    #	Puzzle Game/Library/metadata/23/233a7ea29ab9f0549b7e8fda0ca5b994.info
    #	Puzzle Game/Library/metadata/30/307433eba81a469ab1e2084d52d1a5a2
    #	Puzzle Game/Library/metadata/3a/3aacdd66d48dbc046944c12598f1942e
    #	Puzzle Game/Library/metadata/3c/3c0ad459c1534645b5d603b7cc258f97
    #	Puzzle Game/Library/metadata/41/4113173d5e95493ab8765d7b08371de4
    #	Puzzle Game/Library/metadata/43/43d01a9e938ce3440894af3b4b023939.info
    #	Puzzle Game/Library/metadata/43/43ec2a7ffde66dd449d71082882cd2f6
    #	Puzzle Game/Library/metadata/4b/4ba2329b63d54f0187bcaa12486b1b0f
    #	Puzzle Game/Library/metadata/52/52a206b72a8f4f749c28d8b18b42dd19
    #	Puzzle Game/Library/metadata/53/53ebcfaa2e1e4e2dbc85882cd5a73fa1
    #	Puzzle Game/Library/metadata/5f/5f32cd94baa94578a686d4b9d6b660f7
    #	Puzzle Game/Library/metadata/67/67e9dad5654047ebbe623cce9dbf7b38
    #	Puzzle Game/Library/metadata/6c/6cdf1e5c78d14720aaadccd4c792df96
    #	Puzzle Game/Library/metadata/73/739bbd9f364b4268874f9fd86ab3beef
    #	Puzzle Game/Library/metadata/77/77e986fc7ab54934580d3e8736166230.info
    #	Puzzle Game/Library/metadata/7c/7cbab2be89b54486bbd23a6fe637d30e
    #	Puzzle Game/Library/metadata/80/80a3616ca19596e4da0f10f14d241e9f
    #	Puzzle Game/Library/metadata/85/852e56802eb941638acbb491814497b0
    #	Puzzle Game/Library/metadata/86/86f4de9468454445ac2f39e207fafa3a
    #	Puzzle Game/Library/metadata/87/870353891bb340e2b2a9c8707e7419ba
    #	Puzzle Game/Library/metadata/8e/8e0cd8ed44d4412cbe0642067abc9e44
    #	Puzzle Game/Library/metadata/97/972750b1714134f49b129b3610db8fae.info
    #	Puzzle Game/Library/metadata/97/97decbdab0634cdd991f8d23ddf0dead
    #	Puzzle Game/Library/metadata/ad/adebbd281f1a4ef3a30be7f21937e02f
    #	Puzzle Game/Library/metadata/b3/b306da31995795640bf99fb9267922c9.info
    #	Puzzle Game/Library/metadata/dc/dc443db3e92b4983b9738c1131f555cb
    #	Puzzle Game/Library/metadata/dd/dd99d4b0bfd6a0a439472286d47e4556.info
    #	Puzzle Game/Library/metadata/f1/f15efb703bd5c814a8e61d851c9f5456
    #	Puzzle Game/Library/metadata/f4/f48df919adea4fa095a7407e773e5aa4
    #	Puzzle Game/Library/metadata/f5/f5f67c52d1564df4a8936ccd202a3bd8
    #	Puzzle Game/Library/metadata/f7/f70555f144d8491a825f0804e09c671c
    #	Puzzle Game/Library/metadata/f7/f7b54ff4a43d4fcf81b4538b678e0bcc
    #	Puzzle Game/Puzzle Game.userprefs

---
+++ Puzzle Game/Assets/Scripts/LeverController.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class LeverController : MonoBehaviour {
+
+	public GameObject lightBox;
+	public GameObject leverHand;
+	public bool state;
+	public float tiltAngle = 20.0F;
+	//public float smooth = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+		setLight ();
+		setInitialLeverHand ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public string getType(){
+		ObjectController a = lightBox.GetComponent<ObjectController> ();
+		if (a.isBlueLight)
+			return "BlueLight";
+		else if (a.isRedLight)
+			return "RedLight";
+		else if (a.isYellowLight)
+			return "YellowLight";
+		else if (a.isWhiteLight)
+			return "WhiteLight";
+		else
+			return null;
+	}
+
+
+	public void changeState(){
+		state = !state;
+		Debug.Log ("Lever state changed from "+!state+" to "+state);
+		setLeverhand ();
+		setLight ();
+	}
+
+	public void setLight(){
+		string type = getType ();
+		if (type == "WhiteLight") {
+			Debug.Log ("White light on = " + state);
+			for (int i = 0; i < 4; i++) {
+				if (lightBox.GetComponent<WhiteLight> ().LightTriggerDirection [i])
+					lightBox.GetComponent<WhiteLight> ().lightOn [i] = state;
+			}
+		} else if (type == "RedLight") {
+			Debug.Log ("Red light on = " + state);
+			for (int i = 0; i < 4; i++) {
+				if (lightBox.GetComponent<RedLight> ().LightTriggerDirection [i])
+					lightBox.GetComponent<RedLight> ().lightOn [i] = state;
+			}
+		} else if (type == "BlueLight") {
+			Debug.Log ("Blue light on = " + state);
+			for (int i = 0; i < 4; i++) {
+				if (lightBox.GetComponent<BlueLight> ().LightTriggerDirection [i])
+					lightBox.GetComponent<BlueLight> ().lightOn [i] = state;
+			}
+		} else if (type == "YellowLight") {
+			Debug.Log ("Yellow light on = " + state);
+			for (int i = 0; i < 4; i++) {
+				if (lightBox.GetComponent<YellowLight> ().LightTriggerDirection [i])
+					lightBox.GetComponent<YellowLight> ().lightOn [i] = state;
+			}
+		} else {
+			Debug.Log ("No light script!");
+		}
+	}
+
+	public void setLeverhand(){
+		Debug.Log ("Set Lever Hand");
+		if (!state) {
+			leverHand.transform.RotateAround(transform.position, Vector3.forward, tiltAngle*2);
+		} else {
+			leverHand.transform.RotateAround(transform.position, Vector3.forward, -tiltAngle*2);
+		}
+	}
+
+	public void setInitialLeverHand(){
+		Debug.Log ("Set Initial Lever Hand");
+		if (!state) {
+			leverHand.transform.RotateAround(transform.position, Vector3.forward, tiltAngle);
+		} else {
+			leverHand.transform.RotateAround(transform.position, Vector3.forward, -tiltAngle);
+		}
+	}
+}
