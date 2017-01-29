fd7c32306affcc7362563d7ea2e6643c8ef0f861
JChinnaron <chinnaron1@gmail.com>
2017-01-29 21:07:42 +0700

    Finished Merge

--- Puzzle Game/Assets/Scripts/GoalController.cs
+++ Puzzle Game/Assets/Scripts/GoalController.cs
@@ -6,12 +6,9 @@ public class GoalController : MonoBehaviour {
 
 	//When player stand NEXT to collider box
 	void OnTriggerEnter(Collider other){
-<<<<<<< HEAD
 //		Debug.Log ("Goal Collided");
 		Application.LoadLevel (Application.loadedLevel);
-=======
-		Debug.Log ("Goal Collided");
+//		Debug.Log ("Goal Collided");
 		//Application.LoadLevel (Application.loadedLevel);
->>>>>>> origin/master
 	}
 }
