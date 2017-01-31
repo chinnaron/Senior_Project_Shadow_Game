beb6bbe89a6376429afbae525dcfae3652fc0893
Suraphoj <suraphojb@gmail.com>
2017-01-31 15:58:13 +0700

    UI : Swipe to Rotate

---
+++ Puzzle Game/Assets/SwipeToRotate.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwipeToRotate : MonoBehaviour {
+
+	// Use this for initialization
+	public Vector2 firstPressPos;
+	public Vector2 secondPressPos;
+	public Vector2 currentSwipe;
+	
+	public GameObject bg;
+	public GameObject camera;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+ 
+    if(Input.touches.Length > 0)
+     {
+         Touch t = Input.GetTouch(0);
+         if(t.phase == TouchPhase.Began)
+         {
+              //save began touch 2d point
+             firstPressPos = new Vector2(t.position.x,t.position.y);
+         }
+         if(t.phase == TouchPhase.Ended)
+         {
+              //save ended touch 2d point
+             secondPressPos = new Vector2(t.position.x,t.position.y);
+                           
+              //create vector from the two points
+             currentSwipe = new Vector3(secondPressPos.x - firstPressPos.x, secondPressPos.y - firstPressPos.y);
+               
+             //normalize the 2d vector
+             currentSwipe.Normalize();
+ 
+             //swipe upwards
+             if(currentSwipe.y > 0 && currentSwipe.x > -0.5f && currentSwipe.x < 0.5f)
+             {
+                 Debug.Log("up swipe");
+             }
+             //swipe down
+             if(currentSwipe.y < 0 && currentSwipe.x > -0.5f && currentSwipe.x < 0.5f)
+             {
+                 Debug.Log("down swipe");
+             }
+             //swipe left
+             if(currentSwipe.x < 0&&  currentSwipe.y > -0.5f && currentSwipe.y < 0.5f)
+             {
+                Debug.Log("left swipe");
+				camera.transform.Rotate (new Vector3 (0, 90, 0));
+				bg.transform.Rotate (new Vector3 (0, 90, 0));
+             }
+             //swipe right
+             if(currentSwipe.x > 0 && currentSwipe.y > -0.5f && currentSwipe.y < 0.5f)
+             {
+                 Debug.Log("right swipe");
+				 camera.transform.Rotate (new Vector3 (0, -90, 0));
+				 bg.transform.Rotate (new Vector3 (0, -90, 0));
+             }
+         }
+     }
+	}
+	
+}
