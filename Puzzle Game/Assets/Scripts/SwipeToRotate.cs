39b9f4dac5eca74c93e73b273001109e6ae2cca8
JChinnaron <chinnaron1@gmail.com>
2017-02-02 14:35:27 +0700

    Fixed screen rotation

--- Puzzle Game/Assets/Scripts/SwipeToRotate.cs
+++ Puzzle Game/Assets/Scripts/SwipeToRotate.cs
@@ -10,7 +10,7 @@ public class SwipeToRotate : MonoBehaviour {
 	private Vector2 currentSwipe;
 
 	private Quaternion lookAt;
-	private Quaternion start;
+//	private Quaternion start;
 
 	private readonly float turnSpeed = 10f;
 	private readonly float minSwipeLength = 200f;
@@ -26,24 +26,34 @@ public class SwipeToRotate : MonoBehaviour {
 
 	void Start () {
 		player = FindObjectOfType<PlayerController> ();
-		lookAt = start = camera.transform.rotation;
+		lookAt = camera.transform.rotation;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		DetectSwipe();
 	}
 
 	void FixedUpdate () {
-		camera.transform.rotation = Quaternion.Lerp (start, lookAt, Time.deltaTime * turnSpeed);
-		bg.transform.rotation = Quaternion.Lerp (start, lookAt, Time.deltaTime * turnSpeed);
+		DetectSwipe();
+		camera.transform.rotation = Quaternion.Lerp (camera.transform.rotation, lookAt, Time.deltaTime * turnSpeed);
+		bg.transform.rotation = Quaternion.Lerp (camera.transform.rotation, lookAt, Time.deltaTime * turnSpeed);
 
-		if (camera.transform.rotation == lookAt)
-			moving = false;
+		print (camera.transform.rotation + "" + lookAt);
+		print (camera.transform.rotation.eulerAngles + "" + lookAt.eulerAngles);
+//		if (camera.transform.rotation.eulerAngles - lookAt.eulerAngles)
+//			moving = false;
 	}
 
-	void  DetectSwipe(){
+	Vector3 SetAxis(Quaternion q){
+		if (Mathf.Abs (q.eulerAngles.y - Mathf.Floor (q.eulerAngles.y)) > Mathf.Abs (q.eulerAngles.y - Mathf.Ceil (q.eulerAngles.y)))
+			return new Vector3 (q.eulerAngles.x, Mathf.Ceil (q.eulerAngles.y), q.eulerAngles.z);
+		else
+			return new Vector3 (q.eulerAngles.x, Mathf.Floor (q.eulerAngles.y), q.eulerAngles.z);
+	}
+
+	void DetectSwipe(){
 		if (Input.touches.Length > 0) {
+			print (moving);
 			Touch t = Input.GetTouch (0);
  
 			if (t.phase == TouchPhase.Began) {
@@ -57,36 +67,36 @@ public class SwipeToRotate : MonoBehaviour {
 				// Make sure it was a legit swipe, not a tap
 				if (currentSwipe.magnitude < minSwipeLength) {
 					swipeDirection = Swipe.None;
-					player.Click ();;
+					player.Click ();
 					return;
 				}
            
 				currentSwipe.Normalize ();
-				if (!moving) {
-					// Swipe up
-					if (currentSwipe.y > 0 && currentSwipe.x > -0.5f && currentSwipe.x < 0.5f) {
-						swipeDirection = Swipe.Up;
-						// Swipe down
-					} else if (currentSwipe.y < 0 && currentSwipe.x > -0.5f && currentSwipe.x < 0.5f) {
-						swipeDirection = Swipe.Down;
-						// Swipe left
-					} else if (currentSwipe.x < 0 && currentSwipe.y > -0.5f && currentSwipe.y < 0.5f) {
-						swipeDirection = Swipe.Left;
-						moving = true;
-						lookAt = Quaternion.Euler (camera.transform.rotation.eulerAngles + Vector3.up * -90);
-						start = transform.rotation;
+
+				// Swipe up
+				if (currentSwipe.y > 0 && currentSwipe.x > -0.5f && currentSwipe.x < 0.5f) {
+					swipeDirection = Swipe.Up;
+					// Swipe down
+				} else if (currentSwipe.y < 0 && currentSwipe.x > -0.5f && currentSwipe.x < 0.5f) {
+					swipeDirection = Swipe.Down;
+					// Swipe left
+				} else if (currentSwipe.x < 0 && currentSwipe.y > -0.5f && currentSwipe.y < 0.5f) {
+					swipeDirection = Swipe.Left;
+//						moving = true;
+//					start = camera.transform.rotation;
+					lookAt = Quaternion.Euler (SetAxis (camera.transform.rotation) + Vector3.up * -90);
 //					camera.transform.Rotate (new Vector3 (0, -90, 0));
 //					bg.transform.Rotate (new Vector3 (0, -90, 0));
-						// Swipe right
-					} else if (currentSwipe.x > 0 && currentSwipe.y > -0.5f && currentSwipe.y < 0.5f) {
-						swipeDirection = Swipe.Right;
-						moving = true;
-						lookAt = Quaternion.Euler (camera.transform.rotation.eulerAngles + Vector3.up * 90);
-						start = transform.rotation;
+					// Swipe right
+				} else if (currentSwipe.x > 0 && currentSwipe.y > -0.5f && currentSwipe.y < 0.5f) {
+					swipeDirection = Swipe.Right;
+//						moving = true;
+//					start = camera.transform.rotation;
+					lookAt = Quaternion.Euler (SetAxis (camera.transform.rotation) + Vector3.up * 90);
 //					camera.transform.Rotate (new Vector3 (0, 90, 0));
 //					bg.transform.Rotate (new Vector3 (0, 90, 0));
-					}
 				}
+
 			}
 		} else {
 			swipeDirection = Swipe.None;
