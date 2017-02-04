983c0665aae59214fc1abd148b0ad47d2e0dbb60
JChinnaron <chinnaron1@gmail.com>
2017-02-04 18:08:21 +0700

    Add button

--- Puzzle Game/Assets/Scripts/CameraRotate.cs
+++ Puzzle Game/Assets/Scripts/CameraRotate.cs
@@ -4,27 +4,40 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class CameraRotate : MonoBehaviour {
-	public GameObject bg;
+	private PlayerController player;
 	public GameObject camera;
 	private readonly float turnSpeed = 10f;
 	private Quaternion lookAt;
 
 	// Use this for initialization
 	void Start () {
+		#if UNITY_EDITOR
+		gameObject.SetActive(true);
+		#elif UNITY_ANDROID
+		gameObject.SetActive(false);
+		#endif
+
 		Button btn = GetComponent<Button>();
 		btn.onClick.AddListener(TaskOnClick);
 
-		lookAt = Quaternion.LookRotation (Vector3.forward);
+		player = FindObjectOfType<PlayerController> ();
+		lookAt = camera.transform.rotation;
 	}
 	
 	void TaskOnClick(){
 		//Debug.Log ("You have clicked the button!");
-		lookAt = Quaternion.Euler(transform.rotation.eulerAngles + Vector3.up * 90);
+
+		lookAt = Quaternion.Euler (SetAxis (camera.transform.rotation) + Vector3.up * 90);
+	}
+
+	Vector3 SetAxis(Quaternion q){
+		if (Mathf.Abs (q.eulerAngles.y - Mathf.Floor (q.eulerAngles.y)) > Mathf.Abs (q.eulerAngles.y - Mathf.Ceil (q.eulerAngles.y)))
+			return new Vector3 (q.eulerAngles.x, Mathf.Ceil (q.eulerAngles.y), q.eulerAngles.z);
+		else
+			return new Vector3 (q.eulerAngles.x, Mathf.Floor (q.eulerAngles.y), q.eulerAngles.z);
 	}
 
 	void FixedUpdate(){
-		print(""+lookAt.eulerAngles+transform.rotation.eulerAngles+Quaternion.Slerp (camera.transform.rotation, lookAt, Time.deltaTime * turnSpeed));
-		camera.transform.rotation = Quaternion.Slerp (camera.transform.rotation, lookAt, Time.deltaTime * turnSpeed);
-		bg.transform.rotation = Quaternion.Slerp (bg.transform.rotation, lookAt, Time.deltaTime * turnSpeed);
+		camera.transform.rotation = Quaternion.Lerp (camera.transform.rotation, lookAt, Time.deltaTime * turnSpeed);
 	}
 }
